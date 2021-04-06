81fba489e079c85db3fc67f140848d01bf8fbd68
AmirHKiani <amirhkiani98@yahoo.com>
2021-04-06 09:55:22 +0430

    Change Locations

---
+++ test.py
@@ -0,0 +1,7 @@
+my_string = "string1 is string1. string2 is string2. but string3 is not string3!!!"
+if "string1" in my_string:
+    print("string1 is in my_string")
+if ("string1" in my_string and "string4" in my_string):
+    print("string1 and string4 is in my_string")
+else:
+    print("string1 or string4 so not in my_string")
